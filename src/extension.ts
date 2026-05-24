94c7d49b59e80ceba39b32be51213799464479d4
Brendan <brendanegan20@gmail.com>
2026-05-24 13:31:09 -0400

    Added OOP and Workspace Function looking can handle different languages

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,60 @@
 import * as vscode from 'vscode';
 
-class FunctionItem
+class ClassItem
 extends vscode.TreeItem
 {
     constructor(
         readonly symbol:
             vscode.DocumentSymbol,
-			readonly expanded:
-            boolean
+
+		readonly uri:
+			vscode.Uri
     )
+    {
+        super(
+            symbol.name,
+            vscode.TreeItemCollapsibleState.Collapsed
+        );
+
+        this.contextValue =
+            "classItem";
+    }
+}
+
+class FileItem
+extends vscode.TreeItem
+{
+    constructor(
+        readonly uri:vscode.Uri
+    )
+    {
+        super(
+            vscode.workspace.asRelativePath(uri),
+            vscode.TreeItemCollapsibleState.Collapsed
+        );
+
+        this.contextValue =
+            "fileItem";
+    }
+}
+
+class FunctionItem
+extends vscode.TreeItem
+{
+    constructor(
+		readonly symbol:
+			vscode.DocumentSymbol,
+		readonly uri:
+			vscode.Uri,
+		readonly expanded:
+			boolean
+	)
     {
         super(
             symbol.name,
             expanded
                 ? vscode.TreeItemCollapsibleState.Expanded
-                : vscode.TreeItemCollapsibleState.None
+                : vscode.TreeItemCollapsibleState.Collapsed
         );
 
         this.contextValue =
@@ -23,7 +63,7 @@ extends vscode.TreeItem
         this.command = {
             command:'funcInfo.jump',
             title:'Jump',
-            arguments:[symbol]
+            arguments:[this]
         };
     }
 }
@@ -43,21 +83,55 @@ extends vscode.TreeItem
 }
 
 class FunctionProvider
-implements vscode.TreeDataProvider<FunctionItem | DetailItem>
+implements vscode.TreeDataProvider<FunctionItem | DetailItem | ClassItem | FileItem>
 {
     private refreshEmitter =
         new vscode.EventEmitter<void>();
 
 	private showDetails = true;
 
+	private scanWorkspace = false;
+
+	private view?:vscode.TreeView<any>;
+
     readonly onDidChangeTreeData =
         this.refreshEmitter.event;
+
+	setView(
+		view:
+			vscode.TreeView<any>
+	)
+	{
+		this.view = view;
+	}
 
 	toggleDetails()
 	{
 		this.showDetails =
 			!this.showDetails;
 
+		this.refresh();
+	}
+
+	setFileMode()
+	{
+		this.scanWorkspace = false;
+		if(this.view)
+		{
+			this.view.description =
+				'Active File Functions';
+		}
+		this.refresh();
+	}
+
+	setFolderMode()
+	{
+		this.scanWorkspace = true;
+		if(this.view)
+		{
+			this.view.description =
+				'Workspace Functions';
+		}
 		this.refresh();
 	}
 
@@ -69,7 +143,9 @@ implements vscode.TreeDataProvider<FunctionItem | DetailItem>
     getTreeItem(
 		item:
 			FunctionItem |
-			DetailItem
+			DetailItem   |
+			ClassItem	 |
+			FileItem
 	)
 	{
 		return item;
@@ -81,18 +157,38 @@ implements vscode.TreeDataProvider<FunctionItem | DetailItem>
 			DetailItem
 	)
 	{
-		//
-		// CHILDREN OF FUNCTION NODE
-		//
+		if(element instanceof FileItem)
+		{
+			const symbols =
+				await vscode.commands.executeCommand<
+					vscode.DocumentSymbol[]
+				>(
+					'vscode.executeDocumentSymbolProvider',
+					element.uri
+				);
+
+			if(!symbols)
+				return [];
+
+			return symbols
+				.filter(
+					s =>
+						s.kind === vscode.SymbolKind.Function ||
+						s.kind === vscode.SymbolKind.Method
+				)
+				.map(
+					s =>
+						new FunctionItem(
+							s,
+							element.uri,
+							this.showDetails
+						)
+				);
+		}
 		if(
 			element instanceof FunctionItem
 		)
 		{
-			if(!this.showDetails)
-			{
-				return [];
-			}
-
 			const start =
 				element.symbol.range.start.line + 1;
 
@@ -119,15 +215,63 @@ implements vscode.TreeDataProvider<FunctionItem | DetailItem>
 			];
 		}
 
-		//
-		// ROOT LEVEL
-		//
+		if(
+			element instanceof ClassItem
+		)
+		{
+			return element.symbol.children
+
+				.filter(
+					s =>
+
+					s.kind ===
+						vscode.SymbolKind.Method ||
+
+					s.kind ===
+						vscode.SymbolKind.Function
+				)
+
+				.map(
+					s =>
+
+					new FunctionItem(
+						s,
+						element.uri!,
+						this.showDetails
+					)
+				);
+		}
+
+		if(this.scanWorkspace)
+		{
+			if(
+				!vscode.workspace.workspaceFolders
+			)
+			{
+				vscode.window.showWarningMessage(
+					'No workspace folder open.'
+				);
+
+				return [];
+			}
+
+			const files =
+				await vscode.workspace.findFiles(
+					'**/*.{cpp,h,c,py,ts,js}'
+				);
+
+			return files.map(
+				file =>
+					new FileItem(file)
+			);
+		}
 
 		const editor =
 			vscode.window.activeTextEditor;
 
 		if(!editor)
 		{
+			vscode.window.showInformationMessage( 'No Active File Open.');
 			return [];
 		}
 
@@ -144,23 +288,46 @@ implements vscode.TreeDataProvider<FunctionItem | DetailItem>
 			return [];
 		}
 
-		return symbols
-			.filter(
-				s =>
+		let results:
+		(
+			FunctionItem |
+			ClassItem
+		)[] = [];
 
+		for(const s of symbols)
+		{
+			if(
+				s.kind ===
+				vscode.SymbolKind.Class
+			)
+			{
+				results.push(
+					new ClassItem(
+						s,
+						editor.document.uri
+					)
+				);
+			}
+
+			else if(
 				s.kind ===
 					vscode.SymbolKind.Function ||
 
 				s.kind ===
 					vscode.SymbolKind.Method
 			)
-			.map(
-				s =>
+			{
+				results.push(
 					new FunctionItem(
 						s,
+						editor.document.uri,
 						this.showDetails
 					)
-			);
+				);
+			}
+		}
+
+		return results;
 	}
 }
 
@@ -172,13 +339,76 @@ export function activate(
     const provider =
         new FunctionProvider();
 
-    vscode.window
-        .registerTreeDataProvider(
-            'functionExplorer',
-            provider
-        );
+    const tree =
+    vscode.window.createTreeView(
+        'functionExplorer',
+        {
+            treeDataProvider:
+                provider
+        }
+    );
+
+	provider.setView(tree);
 
     context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'funcInfo.selectFunction',
+			async (
+				item: FunctionItem
+			) =>
+			{
+				const editor =
+					await vscode.window.showTextDocument(
+						item.uri
+					);
+
+				editor.selection =
+					new vscode.Selection(
+						item.symbol.range.start,
+						item.symbol.range.end
+					);
+
+				editor.revealRange(
+					item.symbol.range
+				);
+			}
+		),
+		vscode.commands.registerCommand(
+			'funcInfo.copyFunction',
+			async (
+				item: FunctionItem
+			) =>
+			{
+				const doc =
+					await vscode.workspace.openTextDocument(
+						item.uri
+					);
+
+				const text =
+					doc.getText(
+						item.symbol.range
+					);
+
+				await vscode.env.clipboard
+					.writeText(text);
+			}
+		),
+
+		vscode.commands.registerCommand(
+			'funcInfo.showFile',
+			() =>
+			{
+				provider.setFileMode();
+			}
+		),
+
+		vscode.commands.registerCommand(
+			'funcInfo.showFolder',
+			() =>
+			{
+				provider.setFolderMode();
+			}
+		),
 
 		vscode.commands.registerCommand(
 			'funcInfo.refresh',
@@ -210,27 +440,23 @@ export function activate(
 
         vscode.commands.registerCommand(
             'funcInfo.jump',
-            (
-                symbol:
-                    vscode.DocumentSymbol
+            async (
+                item: FunctionItem
             ) =>
             {
                 const editor =
-                    vscode.window.activeTextEditor;
-
-                if(!editor)
-                {
-                    return;
-                }
+					await vscode.window.showTextDocument(
+						item.uri
+					);
 
                 editor.selection =
                     new vscode.Selection(
-                        symbol.selectionRange.start,
-                        symbol.selectionRange.start
+                        item.symbol.selectionRange.start,
+                        item.symbol.selectionRange.start
                     );
 
                 editor.revealRange(
-                    symbol.range
+                    item.symbol.range
                 );
             }
         )
